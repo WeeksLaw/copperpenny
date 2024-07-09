b4e9e8d29863b9940113365cdc29fe41808f4f43
Lacey <lweeks@weeksconsulting.us>
2024-07-09 10:54:54 -0500

    Initial commit

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from pages import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('pages/contact.html', views.contact, name='contact'),
+    path('pages/about.html', views.about, name='about'),
+    path('pages/portfolio.html', views.blog, name='portfolio'),
+]
---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "pages/home.html", {})
+
+def contact(request):
+    return render(request, "pages/contact.html", {})
+
+def about(request):
+    return render(request, "pages/about.html", {})
+
+def blog(request):
+    return render(request, "pages/portfolio.html", {})
